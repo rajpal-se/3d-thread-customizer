50bdeda35220d5bc79897a4619dd244382190950
Rajpal Singh <rajpal426a@gmail.com>
2023-05-28 09:02:14 +0530

    Initial Setup done.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>Hi there!</div>
+  )
+}
+
+export default App
