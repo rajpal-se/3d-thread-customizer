1ab86bb99339be52accf7663d323fa45923c7c12
Rajpal Singh <rajpal426a@gmail.com>
2023-05-30 22:09:18 +0530

    Home page added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import Canvas from './canvas';
-import Customizer from './pages/Customizer';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div>Hi there!</div>
+    <main className="app transition-all ease-in">
+      <Home />
+    </main>
   )
 }
 
